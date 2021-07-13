05e94646fc0b60b8e16345f6566a830fa80d6025
Leonardo Lopes dos Santos <leonardo.lopes@linkle.io>
2021-07-13 12:54:15 -0300

    🧸 ajuste no cors

--- src/main.py
+++ src/main.py
@@ -5,9 +5,17 @@ from src.controllers.orchestrator import webhook
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
