cf651aa39daa434a968f37d09116140ac8b55940
Leonardo Lopes dos Santos <leonardo.lopes@linkle.io>
2021-07-13 22:04:00 -0300

    📟 cors no response

--- src/controllers/orchestrator.py
+++ src/controllers/orchestrator.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 
 from src.services.orchestrator import webhook_page
 from src.models.interfaces import WebEvent
@@ -6,8 +6,8 @@ from src.models.interfaces import WebEvent
 webhook = APIRouter()
 
 @webhook.post('/')
-def webhook_frontend(request: WebEvent):
+def webhook_frontend(request: WebEvent, response: Response):
     print(request)
     webhook_page(request)
-
+    response.headers["Access-Control-Allow-Origin"] = "*"
     return []
